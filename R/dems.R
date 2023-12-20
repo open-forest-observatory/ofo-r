52fc93dcd828f755cafcfe85af7eeb0bf6707e56
Derek Young <22568494+youngdjn@users.noreply.github.com>
2023-12-20 13:51:15 -0800

    Package framework

--- R/dems.R
+++ R/dems.R
@@ -1,3 +1,12 @@
+#' Calculate Canopy Height Model (CHM) from a coregistered DSM and DTM
+#'
+#' This function calculates the Canopy Height Model (CHM) by subtracting the Digital Terrain Model
+#' (DTM) from the Digital Surface Model (DSM).
+#'
+#' @param dsm A terra SpatRaster representing the Digital Surface Model.
+#' @param dtm A terra SpatRaster representing the Digital Terrain Model.
+#' @return A terra SpatRaster representing the Canopy Height Model.
+#' @export
 chm_from_coregistered_dsm_dtm = function(dsm, dtm) {
   chm = dsm - dtm
   chm[chm < 0] = 0
