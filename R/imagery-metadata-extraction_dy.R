11c64d3f746fc12f7cea40ae3136adf64a67c31f
Derek Young <22568494+youngdjn@users.noreply.github.com>
2024-01-20 15:04:56 -0800

    Modularize individual metadata extraction tasks

--- R/imagery-metadata-extraction_dy.R
+++ R/imagery-metadata-extraction_dy.R
@@ -1,65 +1,43 @@
 # Functions to extract metadata from a drone imagery dataset, specifically using a data frame of
-# EXIF data that has already been created by extracting EXIF data from all the images.
+# EXIF data that has already been created by extracting EXIF data from all the images. Note that the
+# last function defined in this script is a function that wraps all the individual extraction
+# functions to extract all the metadata attributes at once.
 
-# Derek's metadata extraction code
-extract_metadata_dy = function(exif_filepath) {
+# Flight speed
+extract_flight_speed = function(exif) {
 
-  # --- Prep ---
-  # (These are steps that provide setup for extraction of multiple different metadata attributes,
-  # but the actual extraction happens in the subsequent section(s).)
-
-  # Read in the EXIF data from file for the provided dataset filepath, as a data frame
-  exif = read.csv(exif_filepath)
-
-  # Get the dataset ID from the filename (knowing the naming convention is "exif_<dataset_id>.csv")
-  dataset_id = stringr::str_extract(basename(exif_filepath), "(?<=exif_)(.*)(?=\\.csv)")
-  # Alternatively, if we wanted to match the convention for the dataset_id, it would be "(([0-9]){8}-([0-9]){4})"
-
-  # Convert the data frame into a geospatial 'sf' object (you could alternatively use a 'terra::vect'
-  # object)
-  exif = sf::st_as_sf(exif, crs = 4326, coords = c("GPSLongitude", "GPSLatitude"))
-
-  # Convert the capture datetime to an R 'lubridate' object. This uses the EXIF attribute 'DateTimeOriginal', but it
-  # looks like there is another one called 'CreateDate' which has the same info. The lubridate parsing
-  # function assumes the time zone is UTC when not specified (the EXIF attribute doesn't include the TZ), but we will drop that later and
-  # interpret this as local time.
-  exif$capture_datetime = lubridate::ymd_hms(exif$DateTimeOriginal)
-
-  # Arrange images by capture time (presumably they're already in capture order, but just to be sure).
-  # First arrange by full file path (assuming that is the capture order), then by capture time. This
-  # way, the capture time is used as top priority, with the file path used as a tiebreaker (e.g. if
-  # there were two images taken in the same second). The file path where the drone saves the image
-  # onto the SD card (at least for the DJI datasets tested) seems to be stored in EXIF attribute
-  # 'ImageDescription'. We can't use the path alone because if the mission was split over two SD
-  # cards, the file write path may have started over.
-  # TODO: Deal with the case where a dataset was collected by two drones flying at once.
-  exif = exif[order(exif$ImageDescription), ]
-  exif = exif[order(exif$capture_datetime), ]
-
-  # Plot the flight path as a visual check
-  flightpath = exif |> dplyr::summarize(do_union = FALSE) |> sf::st_cast("LINESTRING")
-  plot(flightpath)
-
-  # --- Compute mean flight speed ---
-
-  # Get distance from each image to the next, in meters.
+  # Get distance from each image to the next, in meters
   start_image = exif[1:(nrow(exif) - 1), ]
   end_image = exif[2:nrow(exif), ]
   distance = sf::st_distance(start_image, end_image, by_element = TRUE)
 
-  # Get time from each image to the next, in seconds.
+  # Get time from each image to the next, in seconds
   interval = end_image$capture_datetime - start_image$capture_datetime
 
   # Compute speed in meters per second
   speed = as.numeric(distance) / as.numeric(interval)
 
-  # Compute the median speed as the value to report, to avoid influence of outliers (such as battery swaps)
+  # Compute the median speed as the value to report, to avoid influence of outliers (such as battery
+  # swaps)
   speed = median(speed, na.rm = TRUE)
 
+  return(speed)
 
-  # --- Return extracted/computed metadata as a data frame row ---
-  metadata = data.frame(dataset_id = dataset_id,
-                        flight_speed_derived = speed
+}
+
+# Wrapper for Derek's metadata extraction functions. Preps the EXIF data for passing to the
+# extraction functions, then calls all the individual extraction functions to extract the respecive attributes.
+extract_metadata_dy = function(exif_filepath, plot_flightpath = FALSE) {
+
+  # Prep the EXIF data for extraction of metadata attributes
+  exif = prep_exif(exif_filepath, plot_flightpath = plot_flightpath)
+
+  # Extract/compute metadata attributes
+  flight_speed_derived = extract_flight_speed(exif)
+
+  # Return extracted/computed metadata as a data frame row
+  metadata = data.frame(dataset_id = exif$dataset_id[1],
+                        flight_speed_derived = flight_speed_derived
                         # Add more metadata variables here
                         )
 
---
+++ R/imagery-metadata-extraction_general.R
@@ -0,0 +1,44 @@
+# Prep the EXIF data for extraction of metadata attributes. Speficially, load the EXIF CSV file into
+# a dataframe, add a dataset_id attribute based on the filename, compute image capture datetime, order the images by capture time, and optionally plot
+# the flight path for visual inspection
+prep_exif = function(exif_filepath, plot_flightpath = FALSE) {
+
+  # Read in the EXIF data from file for the provided dataset filepath, as a data frame
+  exif = read.csv(exif_filepath)
+
+  # Get the dataset ID from the filename (knowing the naming convention is "exif_<dataset_id>.csv")
+  exif$dataset_id = stringr::str_extract(basename(exif_filepath), "(?<=exif_)(.*)(?=\\.csv)")
+  # Alternatively, if we wanted to match the convention for the dataset_id, it would be
+  # "(([0-9]){8}-([0-9]){4})"   # nolint
+
+  # Convert the data frame into a geospatial 'sf' object (you could alternatively use a
+  # 'terra::vect' object)
+  exif = sf::st_as_sf(exif, crs = 4326, coords = c("GPSLongitude", "GPSLatitude"))
+
+  # Convert the capture datetime to an R 'lubridate' object. This uses the EXIF attribute
+  # 'DateTimeOriginal', but it looks like there is another one called 'CreateDate' which has the
+  # same info. The lubridate parsing function assumes the time zone is UTC when not specified (the
+  # EXIF attribute doesn't include the TZ), but we will drop that later and interpret this as local
+  # time.
+  exif$capture_datetime = lubridate::ymd_hms(exif$DateTimeOriginal)
+
+  # Arrange images by capture time (presumably they're already in capture order, but just to be
+  # sure). First arrange by full file path (assuming that is the capture order), then by capture
+  # time. This way, the capture time is used as top priority, with the file path used as a
+  # tiebreaker (e.g. if there were two images taken in the same second). The file path where the
+  # drone saves the image onto the SD card (at least for the DJI datasets tested) seems to be stored
+  # in EXIF attribute 'ImageDescription'. We can't use the path alone because if the mission was
+  # split over two SD cards, the file write path may have started over. TODO: Deal with the case
+  # where a dataset was collected by two drones flying at once.
+  exif = exif[order(exif$ImageDescription), ]
+  exif = exif[order(exif$capture_datetime), ]
+
+  if(plot_flightpath) {
+    # Plot the flight path as a visual check
+    flightpath = exif |> dplyr::summarize(do_union = FALSE) |> sf::st_cast("LINESTRING")
+    plot(flightpath)
+  }
+
+  return(exif)
+
+}
