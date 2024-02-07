757ea6fd77e2ed5f25d06e47aaf19504083d5069
Derek Young <22568494+youngdjn@users.noreply.github.com>
2024-02-07 20:56:57 +0000

    Exclude derived configs

--- sandbox/metashape-tuning/prep-configs.R
+++ sandbox/metashape-tuning/prep-configs.R
@@ -11,11 +11,11 @@ scenarios = expand.grid("filterPointsUSGS__enabled" = c(FALSE),
                         "buildDepthMaps__max_neighbors" = c(60),
                         "buildModel__face_count" = c("Metashape.LowFaceCount", "Metashape.MediumFaceCount", "Metashape.HighFaceCount"))
 name = paste("cfg", stringr::str_pad(1:nrow(scenarios), 3, pad = "0"), sep = "")
-scenarios$photo_path = "/ofo-share/metashape-tuning/raw-images-subset/delta1/"
-scenarios$config_filename = paste0("tuning01_", name)
+scenarios$photo_path = "/ofo-share/metashape-tuning/raw-images-subset/delta2/"
+scenarios$config_filename = paste0("tuning01d2_", name)
 scenarios01 = scenarios
 
-base_yaml_filepath = file.path(getwd(), "sandbox", "metashape-tuning", "configs", "01", "base.yml")
+base_yaml_filepath = file.path(getwd(), "sandbox", "metashape-tuning", "configs", "01d2", "base.yml")
 
 make_derived_configs(base_yaml_filepath, scenarios01,
                      metashape_path = "/ofo-share/utils/automate-metashape/python/metashape_workflow.py")
