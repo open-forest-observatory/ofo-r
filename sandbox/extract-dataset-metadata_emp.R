4a364b43c6ebe9b045266491de6c55b8c0043d0e
Emily Marie Purvis <emily.m.purvis@ucdavis.edu>
2024-04-24 19:46:03 -0700

    sandbox file for dataset metadata (emp edition) complete; output files on Box

--- sandbox/extract-dataset-metadata_emp.R
+++ sandbox/extract-dataset-metadata_emp.R
@@ -6,6 +6,7 @@
 
 library(stringr)
 library(tidyverse)
+library(sf)
 library(devtools)
 
 # Set working directory
@@ -277,15 +278,15 @@ exposure_stdev_derived
 
 # BEGIN FUNCTION CODE
 
-image_density =
+image_density = (nrow(exif))/(units::set_units(sf::st_area(create_mission_polygon(exif, image_merge_distance)), "hectare"))
 
 # END FUNCTION CODE
 
 # turn code into a function
 
-extract_image_density = function (exif) {
+extract_image_density = function (exif, image_merge_distance) {
 
-  image_density =
+  image_density = (nrow(exif))/(units::set_units(sf::st_area(create_mission_polygon(exif, image_merge_distance)), "hectare"))
 
   return(image_density)
 
@@ -293,7 +294,7 @@ extract_image_density = function (exif) {
 
 # test function on exif data
 
-image_density = extract_image_density(exif)
+image_density = extract_image_density(exif, image_merge_distance = 50)
 
 image_density
 
@@ -303,15 +304,15 @@ image_density
 
 # BEGIN FUNCTION CODE
 
-area_ha =
+area_ha = units::set_units(sf::st_area(create_mission_polygon(exif, image_merge_distance)), "hectare")
 
 # END FUNCTION CODE
 
 # turn code into a function
 
-extract_area_ha = function (exif) {
+extract_area_ha = function (exif, image_merge_distance) {
 
-  area_ha =
+  area_ha = units::set_units(sf::st_area(create_mission_polygon(exif, image_merge_distance)), "hectare")
 
   return(area_ha)
 
@@ -319,6 +320,93 @@ extract_area_ha = function (exif) {
 
 # test function on exif data
 
-area_ha = extract_area_ha(exif)
+area_ha = extract_area_ha(exif, image_merge_distance = 50)
 
 area_ha
+
+#### Create wrapper for metadata extraction functions. Preps the EXIF data for passing to the extraction functions, then calls all the individual extraction functions to extract the respective attributes. ####
+
+extract_dataset_metadata_emp = function(exif_filepath, image_merge_distance) {
+
+  # Prep the EXIF data for extraction of metadata attributes
+  exif = prep_exif(exif_filepath)
+
+  # Extract/compute metadata attributes
+  dataset_id = extract_dataset_id(exif)
+  image_count = extract_image_count(exif)
+  file_size = extract_file_size(exif)
+  percent_images_rtk = extract_percent_images_rtk(exif)
+  white_balance_mode_derived = extract_white_balance_mode_derived(exif)
+  white_balance_mode_prop_derived = extract_white_balance_mode_prop_derived(exif)
+  exposure_median_derived = extract_exposure_median_derived(exif)
+  exposure_stdev_derived = extract_exposure_stdev_derived(exif)
+  image_density = extract_image_density(exif, image_merge_distance)
+  area_ha = extract_area_ha(exif, image_merge_distance = 50)
+
+  # Return extracted/computed metadata as a data frame row
+  metadata = data.frame(dataset_id = dataset_id,
+                        image_count = image_count,
+                        file_size = file_size,
+                        percent_images_rtk = percent_images_rtk,
+                        white_balance_mode_derived = white_balance_mode_derived,
+                        white_balance_mode_prop_derived = white_balance_mode_prop_derived,
+                        exposure_median_derived = exposure_median_derived,
+                        exposure_stdev_derived = exposure_stdev_derived,
+                        image_density = image_density,
+                        area_ha = area_ha
+  )
+
+  return(metadata)
+
+}
+
+#### Workflow for running metadata extraction ####
+
+exif_files = list.files(file.path(datadir, "exif-examples"), pattern = "^exif.+\\.csv$", full.names = TRUE)
+
+out_dir = file.path(datadir, "extracted-metadata", "image-level-metadata")
+
+if (!dir.exists(out_dir)) dir.create(out_dir, recursive = TRUE)
+
+# Extract metadata for one EXIF file at a time and save to csv
+
+exif_file = exif_files[1]
+
+metadata1 <- extract_dataset_metadata_emp(exif_file, image_merge_distance = 50)
+
+write.csv(metadata1, file.path("C:\\Users\\emily\\Box\\imagery-metadata-dev/extracted-metadata/dataset-level-tabular/dataset-metadata-emp_20220630-0041.csv"), row.names = FALSE)
+
+
+exif_file = exif_files[2]
+
+metadata2 <- extract_dataset_metadata_emp(exif_file, image_merge_distance = 50)
+
+write.csv(metadata2, file.path("C:\\Users\\emily\\Box\\imagery-metadata-dev/extracted-metadata/dataset-level-tabular/dataset-metadata-emp_20220730-0079.csv"), row.names = FALSE)
+
+
+exif_file = exif_files[3]
+
+metadata3 <- extract_dataset_metadata_emp(exif_file, image_merge_distance = 50)
+
+write.csv(metadata3, file.path("C:\\Users\\emily\\Box\\imagery-metadata-dev/extracted-metadata/dataset-level-tabular/dataset-metadata-emp_20230528-0008.csv"), row.names = FALSE)
+
+
+exif_file = exif_files[4]
+
+metadata4 <- extract_dataset_metadata_emp(exif_file, image_merge_distance = 50)
+
+write.csv(metadata4, file.path("C:\\Users\\emily\\Box\\imagery-metadata-dev/extracted-metadata/dataset-level-tabular/dataset-metadata-emp_20230528-0009.csv"), row.names = FALSE)
+
+
+exif_file = exif_files[5]
+
+metadata5 <- extract_dataset_metadata_emp(exif_file, image_merge_distance = 50)
+
+write.csv(metadata5, file.path("C:\\Users\\emily\\Box\\imagery-metadata-dev/extracted-metadata/dataset-level-tabular/dataset-metadata-emp_20230706-0152.csv"), row.names = FALSE)
+
+
+exif_file = exif_files[6]
+
+metadata6 <- extract_dataset_metadata_emp(exif_file, image_merge_distance = 50)
+
+write.csv(metadata6, file.path("C:\\Users\\emily\\Box\\imagery-metadata-dev/extracted-metadata/dataset-level-tabular/dataset-metadata-emp_20230706-0153.csv"), row.names = FALSE)
