85ba0e0ae61bc1184f1641bbf512a8e348d8b44d
Steven dePaschalis <sdepaschalis@ucdavis.edu>
2024-04-28 16:19:24 -0700

    Update to pitch component of the script

--- sandbox/imagery-metadata-extraction_sd2.R
+++ sandbox/imagery-metadata-extraction_sd2.R
@@ -17,20 +17,28 @@ process_pitch_values <- function(pitch_values) {
   # Exclude pitch values within ±2 degrees of Nadir (-90 degrees)
   filtered_pitch_values = pitch_values[abs(pitch_values + 90) > 2]
 
-  # Check if the filtered_pitch_values contains any NA values
-  if (any(is.na(filtered_pitch_values))) {
-    # Print a warning message and return NA if NA values are present
-    warning("Some pitch values are NA. Skipping calculation for this file.")
-    return(NA)
-  }
+  # Compute 0.1, 0.5, and 0.9 quantiles
+  quantiles = quantile(filtered_pitch_values, c(0.1, 0.5, 0.9), na.rm = TRUE)  # Add na.rm = TRUE to handle NA values
 
-  # Check if the mission is smart oblique based on the presence of three different pitch values
-  if (length(unique(filtered_pitch_values)) == 3) {
-    # Compute the absolute values and then average them for smart oblique shots
-    processed_pitch = mean(abs(filtered_pitch_values), na.rm = TRUE)
+  # Check if quantiles contains valid values
+  if (!any(is.na(quantiles))) {
+    # Check if the range between 0.1 and 0.9 quantiles is at least 10 degrees
+    pitch_range = quantiles[3] - quantiles[1]
+    if (pitch_range >= 10) {
+      # Check if the absolute values of 0.1 and 0.9 quantiles are similar
+      if (abs(quantiles[1]) - abs(quantiles[3]) < 5) {
+        # Use 0.1 or 0.9 quantile directly as the mission pitch
+        processed_pitch = quantiles[1]
+      } else {
+        # Compute the average of absolute pitch values for smart oblique missions
+        processed_pitch = mean(abs(filtered_pitch_values), na.rm = TRUE)
+      }
+    } else {
+      # Use the regular mean for non-smart oblique missions
+      processed_pitch = mean(filtered_pitch_values, na.rm = TRUE)
+    }
   } else {
-    # Use the regular mean for non-smart oblique missions
-    processed_pitch = mean(filtered_pitch_values, na.rm = TRUE)
+    processed_pitch = NA  # Set processed_pitch to NA if quantiles are not valid
   }
 
   return(processed_pitch)
@@ -47,6 +55,7 @@ extract_metadata <- function(exif) {
 
   # Process pitch values
   camera_pitch_values = exif$CameraPitch
+  camera_pitch_values = as.numeric(camera_pitch_values)  # Convert to numeric if necessary
   camera_pitch_derived = process_pitch_values(camera_pitch_values)
 
   # Convert start and end datetime_local to the desired format (YYYYMMDD HHMMSS)
@@ -72,5 +81,3 @@ extracted_metadata_df <- do.call(rbind, extracted_metadata)
 
 # Print the extracted metadata
 print(extracted_metadata_df)
-
-
