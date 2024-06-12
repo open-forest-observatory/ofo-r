ee62a98ffd8b624574471f6b4f1224bac6427642
Derek Young <22568494+youngdjn@users.noreply.github.com>
2024-06-12 05:22:15 +0000

    Export alternative ITD function

--- R/individual-tree-detection.R
+++ R/individual-tree-detection.R
@@ -33,6 +33,17 @@ detect_trees <- function(chm, ws) {
 }
 
 
+#' Delineate individual trees from canopy height model raster
+#'
+#' This function uses pixel based height parameters and their distribution
+#' around a given neighborhood to find individual tree locations and later the tree crown area and tree heights.
+#'
+#' @param chm A terra SpatRaster representing the canopy height model.
+#' @param ws moving window size. This indicates as number of pixels to consider.
+#' @return A spatial point DataFrame representing the tree locations, height, and tree ID.
+#' @examples
+#'
+#' @export
 detect_trees2 = function(chm,
                          ws,
                          algorithm = lidR::lmf(ws = ws),
