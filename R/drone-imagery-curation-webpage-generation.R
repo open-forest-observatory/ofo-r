b896d1ae82da05c2805b60bc4fcc8948b93e84d7
Derek Young <22568494+youngdjn@users.noreply.github.com>
2024-08-31 00:59:45 +0000

    Partial progress porting ground reference catalog creation to drone imagery missions

---
+++ R/drone-imagery-curation-webpage-generation.R
@@ -0,0 +1,65 @@
+
+# Pull in all attributes to the plot summary table, compute relevant columns for display, (like
+# plot area in ha instead of m2), round numeric columns, create links to project and dataset pages
+compile_mission_summary_table = function(mission_level_metadata, base_ofo_url, mission_details_dir) {
+
+  # TODO: join relevant data to the mission metadata
+
+  # Select relevant columns to display
+  d = mission_level_metadata |>
+    dplyr::mutate(overlap_combined_nominal = paste(overlap_front_nominal, overlap_side_nominal, sep = "/"),
+           license = "License will display here")
+
+  # Create links to project and dataset pages
+  d = d |>
+    dplyr::mutate(dataset_id_link = paste0('<a href="', base_ofo_url, mission_details_dir, dataset_id, '/"', ' target="_PARENT">', dataset_id, "</a>"))
+
+  return(d)
+
+}
+
+
+
+make_mission_catalog_datatable = function(mission_summary,
+                                       website_static_path,
+                                       datatable_header_files_dir,
+                                       mission_catalog_datatable_dir,
+                                       mission_catalog_datatable_filename) {
+
+  d = mission_summary |>
+    select("ID" = dataset_id_link,
+           "Area (ha)" = area_derived,
+           "Date" = earliest_date_derived,
+           "Altitude (m) (N)" = altitude_agl_nominal,
+           "Overlap (N)" = overlap_combined_nominal,
+           "Camera pitch" = camera_pitch_derived,
+           "Terrain follow (N)" = terrain_follow,
+           "Flight pattern" = flight_pattern,
+           "Image count" = n_images,
+           "RTK (N)" = rtk_nominal,
+           "RTK images (%)" = percent_images_rtk_derived,
+           "Contributor dataset name" = contributor_dataset_name,
+           "Project" = project_id,
+           "Aircraft" = aircraft_model_name,
+           "License" = license)
+
+  # Prep formatting code to pass to datatable creation
+  format_js = DT::JS("function(settings, json) {",
+                     "$('body').css({'font-family': 'Arial'});",
+                     "}")
+
+  dt = DT::datatable(d,
+                     escape = FALSE,
+                     options = list(paging = FALSE, initComplete = format_js))
+
+  # Save the datatable HTML to the website repo
+  save_widget_html(dt,
+                   website_static_path = website_static_path,
+                   header_files_dir = datatable_header_files_dir,
+                   html_dir = mission_catalog_datatable_dir,
+                   html_filename = mission_catalog_datatable_filename,
+                   delete_folder_first = TRUE)
+
+  return(dt)
+
+}
--- sandbox/drone-imagery-curation/create-drone-imagery-curation-webpages.R
+++ sandbox/drone-imagery-curation/create-drone-imagery-curation-webpages.R
@@ -1,4 +1,4 @@
-# Purpose: Create the Hugo markdown pages for all field reference sites, as well as a dataset index
+# Purpose: Create the Hugo markdown pages for all drone imagery datasets, as well as a dataset index
 # page
 
 library(dplyr)
@@ -12,93 +12,63 @@ library(jinjar)
 devtools::load_all()
 
 
-datadir = readLines("sandbox/data-dirs/derek-groundref-laptop.txt", n = 1)
-
-plot_boundaries_data_dir = file.path(datadir, "field-plot-boundaries")
-google_sheet_id = "1GjDseDCR1BX_EIkJAni7rk2zvK6nHmZz1nOFBd1d6k4"
-
 BASE_OFO_URL = "https://openforestobservatory.org/"
 #BASE_OFO_URL = "http://localhost:1313/"
-WEBSITE_REPO_PATH = "~/repos/ofo-website-3/"
+WEBSITE_REPO_PATH = "/ofo-share/repos-derek/ofo-website-3"
 
 # Path to the plot details template page within theis repo
-PLOT_DETAILS_TEMPLATE_FILEPATH = fs::path(file.path("sandbox", "ground-ref-data", "templates", "ground-ref-plot-details.md"))
+MISSION_DETAILS_TEMPLATE_FILEPATH = fs::path(file.path("sandbox", "drone-imagery-curation", "templates", "drone-mission-details.md"))
 
 # Path to plot details dir relative to the 'content' dir in the website repo. No leading slash but
 # trailing slash
-PLOT_DETAILS_PAGE_DIR = "data/ground-ref/plot-details/"
+MISSION_DETAILS_PAGE_DIR = "data/drone/curation/mission-details/"
 
 # Path to static website files dirs within website repo relative to 'static'. Leading slash but no
 # trailing slash
-DATATABLE_HEADER_FILES_DIR = "/datatable-header-files"
-LEAFLET_HEADER_FILES_DIR = "/leaflet-header-files"
-PLOT_CATALOG_DATATABLE_DIR = "/ground-plot-catalog-datatable/"
-PLOT_CATALOG_DATATABLE_FILENAME = "ground-plot-catalog-datatable.html"
-PLOT_CATALOG_MAP_DIR = "/ground-plot-catalog-map/"
-PLOT_CATALOG_MAP_FILENAME = "ground-plot-catalog-map.html"
-PLOT_DETAILS_DATATABLE_DIR = "/ground-plot-details-datatables"
-PLOT_DETAILS_MAP_DIR = "/ground-plot-details-maps"
+DATATABLE_HEADER_FILES_DIR = "/datatable-header-files_curation"
+LEAFLET_HEADER_FILES_DIR = "/leaflet-header-files_curation"
+MISSION_CATALOG_DATATABLE_DIR = "/curation-mission-catalog-datatable/"
+MISSION_CATALOG_DATATABLE_FILENAME = "curation-mission-catalog-datatable.html"
+MISSION_CATALOG_MAP_DIR = "/curation-mission-catalog-map/"
+MISSION_CATALOG_MAP_FILENAME = "curation-mission-catalog-map.html"
+MISSION_DETAILS_DATATABLE_DIR = "/curation-mission-details-datatables"
+MISSION_DETAILS_MAP_DIR = "/curation-mission-details-maps"
 
 WEBSITE_STATIC_PATH = file.path(WEBSITE_REPO_PATH, "static", "")
 WEBSITE_CONTENT_PATH = file.path(WEBSITE_REPO_PATH, "content", "")
 
-
-# --- Notes on data issues:
-# - tree 4310 (VP dataset) has `crown_position` of 7, which is nonsensical and translates to ohvis
-#   of 7
-# - ******* Johnston plot cleaning
-
-# --- TODO:
-# - Options to color markers by project? Plot size? Etc.:
-#   https://stackoverflow.com/questions/47966375/how-to-create-leaflet-markers-colored-by-a-numeric-variable
-# - Make sure plots with zero trees are not being removed
-
+# Data paths
+MISSION_POLYGONS_PATH = "/ofo-share/drone-imagery-organization/3c_metadata-extracted/all-mission-polygons-w-metadata.gpkg"
+MISSION_POINTS_PATH = "/ofo-share/drone-imagery-organization/3c_metadata-extracted/all-mission-points-w-metadata.gpkg"
 
 
 # ---- Processing
 
-# Load and prep field ref data
-
-tabular_data = read_and_standardize_tabular_field_ref_data(google_sheet_id)
-bounds = read_and_merge_plot_boundaries(plot_boundaries_data_dir)
-
-check_field_ref_data(tabular_data, bounds)
-
-trees = prep_trees(tabular_data$trees, tabular_data$species_codes)
-plots = prep_plots(tabular_data$plots)
-
-trees_for_plot_summary = prep_trees_for_plot_summary(trees)
-
-plot_level_tree_summary = summarize_trees_by_plot(trees_for_plot_summary)
-
+# Load and prep metadata
+mission_polygons = st_read(MISSION_POLYGONS_PATH)
+mission_points = st_read(MISSION_POINTS_PATH)
 
 # Save header library files required by embedded HTML datatables and leaflet maps
 save_dt_header_files(WEBSITE_STATIC_PATH, DATATABLE_HEADER_FILES_DIR)
 save_leaflet_header_files(WEBSITE_STATIC_PATH, LEAFLET_HEADER_FILES_DIR)
 
+mission_level_metadata = st_drop_geometry(mission_polygons)
 
-plot_summary = compile_plot_summary_table(plots = plots,
-                                          projects = tabular_data$projects,
-                                          plot_level_tree_summary = plot_level_tree_summary,
-                                          bounds = bounds,
-                                          base_ofo_url = BASE_OFO_URL,
-                                          plot_details_dir = PLOT_DETAILS_PAGE_DIR)
-
-
-# Imprecise test for whether the data is still in the process of entry and should be skipped. TODO:
-# Consider turning into a function
-plot_summary = plot_summary |>
-  dplyr::filter(!is.na(plot_id) & !is.na(survey_date))
-
-# Write the plot-level data to a csv (for use otside the website purposes)
-readr::write_csv(plot_summary, file.path(datadir, "field-plot-summaries", "field-plot-summary.csv"))
+# Compile relevant and human-readable values from mission attributes
+mission_summary = compile_mission_summary_table(mission_level_metadata,
+                                                base_ofo_url = BASE_OFO_URL,
+                                                mission_details_dir = MISSION_DETAILS_PAGE_DIR)
 
 # Make a HTML data table of plot catalog
-dt = make_plot_catalog_datatable(plot_summary = plot_summary,
+dt = make_mission_catalog_datatable(mission_summary = mission_summary,
                             website_static_path = WEBSITE_STATIC_PATH,
                             datatable_header_files_dir = DATATABLE_HEADER_FILES_DIR,
-                            plot_catalog_datatable_dir = PLOT_CATALOG_DATATABLE_DIR,
-                            plot_catalog_datatable_filename = PLOT_CATALOG_DATATABLE_FILENAME)
+                            mission_catalog_datatable_dir = MISSION_CATALOG_DATATABLE_DIR,
+                            mission_catalog_datatable_filename = MISSION_CATALOG_DATATABLE_FILENAME)
+
+
+### RESUME HERE
+
 
 # Make leaflet map of field data catalog
 plot_centroids = sf::st_centroid(bounds)
