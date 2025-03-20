8d5dbe6399cc601b13901cf4fb1add2faee15511
Derek <22568494+youngdjn@users.noreply.github.com>
2025-03-20 07:13:50 +0000

    Include ITD among the processed files that are merged to publishable file tree

--- sandbox/drone-imagery-ingestion/28_merge-publishable-files-to-publishable-tree.R
+++ sandbox/drone-imagery-ingestion/28_merge-publishable-files-to-publishable-tree.R
@@ -10,6 +10,7 @@ library(purrr)
 PUBLISHABLE_MISSION_FOOTPRINTS_PATH = "/ofo-share/drone-imagery-processed/01/mission-footprints-publish"
 PUBLISHABLE_IMAGES_PATH = "/ofo-share/drone-imagery-organization/7_to-publish"
 PHOTOGRAMMETRY_PUBLISH_PATH = "/ofo-share/drone-imagery-processed/01/photogrammetry-publish"
+ITD_PUBLISH_PATH = "/ofo-share/drone-imagery-processed/01/tree-detection-publish"
 
 PUBLISHABLE_DATA_TREE = "/ofo-share/drone-data-publish/01/"
 
@@ -57,6 +58,20 @@ file.link(infiles_full, outfiles)
 # First make the directories
 infiles = list.files(PHOTOGRAMMETRY_PUBLISH_PATH, full.names = FALSE, recursive = TRUE)
 infiles_full = file.path(PHOTOGRAMMETRY_PUBLISH_PATH, infiles)
+outfiles = file.path(PUBLISHABLE_DATA_TREE, infiles)
+outdirs = unique(dirname(outfiles))
+
+walk(outdirs, dir.create, recursive = TRUE)
+
+file.link(infiles_full, outfiles)
+
+
+# ITD
+
+# Hardlink all files from the publishable ITD dir to the unified publishable tree
+# First make the directories
+infiles = list.files(ITD_PUBLISH_PATH, full.names = FALSE, recursive = TRUE)
+infiles_full = file.path(ITD_PUBLISH_PATH, infiles)
 outfiles = file.path(PUBLISHABLE_DATA_TREE, infiles)
 outdirs = unique(dirname(outfiles))
 
