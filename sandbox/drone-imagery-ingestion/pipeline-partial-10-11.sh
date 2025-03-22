359fe988a0358c2b9e3815225dc9da3024d77b06
Derek <22568494+youngdjn@users.noreply.github.com>
2025-03-22 01:53:37 +0000

    Remove extraneous script

--- sandbox/drone-imagery-ingestion/pipeline-partial-10-11.sh
+++
@@ -1,5 +0,0 @@
-# This is to run enough of the pipline to update the metadata for already organized images. You need
-# to set the project id in the file sandbox/drone-imagery-ingestion/imagery_project_name.txt first.
-
-Rscript --vanilla /ofo-share/repos-derek/ofo-r/sandbox/drone-imagery-ingestion/10_merge-exif-and-baserow.R
-Rscript --vanilla /ofo-share/repos-derek/ofo-r/sandbox/drone-imagery-ingestion/11_metadata-to-geospatial.R
