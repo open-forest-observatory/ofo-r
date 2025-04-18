205b3ca4d6efaebc9d0198d182d3d68544ed6b0d
Derek <22568494+youngdjn@users.noreply.github.com>
2025-04-18 23:45:45 +0000

    Run at sub-mission level, not project level

--- sandbox/drone-imagery-ingestion/05_combine-subdatasets.R
+++ sandbox/drone-imagery-ingestion/05_combine-subdatasets.R
@@ -1,4 +1,4 @@
-# Purpose: There are many datasets that are composed of subdatasets (e.g., the E-W and N-S flights
+# Purpose: There are many missions that are composed of sub-missions (e.g., the E-W and N-S flights
 # of an oblique grid mission). AT this stage, they have been identified an are stored in the top
 # level of the folder `2_sorted-notcleaned` as, e.g., 000345-01 and 000345-02. Combine those into a
 # single dataset "superfolder", with the subdataset folders beneath it.
@@ -6,33 +6,26 @@
 library(tidyverse)
 library(furrr)
 
-# Handle difference in how the current directory is set between debugging and command line call
-if (file.exists("sandbox/drone-imagery-ingestion/imagery_project_name.txt")) {
-  IMAGERY_PROJECT_NAME_FILE = "sandbox/drone-imagery-ingestion/imagery_project_name.txt"
-} else {
-  IMAGERY_PROJECT_NAME_FILE = "imagery_project_name.txt"
-}
-IMAGERY_PROJECT_NAME = read_lines(IMAGERY_PROJECT_NAME_FILE)
+# In
+SORTED_SUB_MISSIONS_FOLDER = "/ofo-share/drone-imagery-organization/2_sorted-sub-mission"
 
-SORTED_IMAGERY_FOLDER = "/ofo-share/drone-imagery-organization/2_sorted-notcleaned"
-COMBINED_IMAGERY_FOLDER = "/ofo-share/drone-imagery-organization/3_sorted-notcleaned-combined"
-
-sorted_imagery_folder = file.path(SORTED_IMAGERY_FOLDER, IMAGERY_PROJECT_NAME)
-combined_imagery_folder = file.path(COMBINED_IMAGERY_FOLDER, IMAGERY_PROJECT_NAME)
+# Out
+SORTED_MISSIONS_FOLDER = "/ofo-share/drone-imagery-organization/3_sorted-mission"
 
 # Processing
 
 # Load the folder list
 
-folders = list.dirs(sorted_imagery_folder, recursive = FALSE)
+folders = list.dirs(SORTED_SUB_MISSIONS_FOLDER, recursive = FALSE)
 
 d = data.frame(folders)
 
 d = d |>
   mutate(dataset_subdataset = basename(folders)) |>
   mutate(dataset = str_split(dataset_subdataset, "-") |> map(1)) |>
-  mutate(in_path = file.path(sorted_imagery_folder, dataset_subdataset),
-         out_path = file.path(combined_imagery_folder, dataset, dataset_subdataset))
+  mutate(in_path = file.path(SORTED_SUB_MISSIONS_FOLDER, dataset_subdataset),
+         out_path = file.path(SORTED_MISSIONS_FOLDER, dataset, dataset_subdataset))
+
 
 make_hardlinks = function(d_row) {
   in_path = d_row$in_path
